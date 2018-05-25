f56bbcef6bcb24312c67d2637253d069b452e77b
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2018-05-25 12:55:24 -0400

    added user saving and response embed

--- src/main/java/com/npchan/Bot.java
+++ src/main/java/com/npchan/Bot.java
@@ -1,20 +1,21 @@
 package com.npchan;
 
+import com.mongodb.*;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
-import org.json.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import org.javacord.api.entity.message.embed.*;
+import java.net.UnknownHostException;
 
 public class Bot {
     public static void getBeatmap(MessageCreateEvent event, String username, String beatmap) {
@@ -91,5 +92,136 @@ public class Bot {
                 .setFooter("© Riker, Flo, & Tux")
                 .setColor(Color.pink);
         event.getChannel().sendMessage(embed);
+    }
+
+    public static void osuUserApiCall(MessageCreateEvent event, String user,
+                                      String osuUsername) {
+        try {
+//            build httpclient object
+            HttpClient httpClient = HttpClientBuilder.create().build();
+//            api url GET for song data
+            HttpGet getRequest = new HttpGet(
+                    "https://osu.ppy.sh/api/get_user?u=" + osuUsername + "&k=" +
+                            System.getenv("OSU_KEY"));
+            getRequest.addHeader("accept", "application/json");
+//            had to set headers
+            getRequest.setHeader("User-Agent", "Anonymous");
+            HttpResponse response = httpClient.execute(getRequest);
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+
+            String userOutput;
+            while ((userOutput = br.readLine()) != null) {
+                Bot.setUser(event, user, userOutput);
+            }
+
+            httpClient.getConnectionManager().shutdown();
+
+        } catch (ClientProtocolException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public static void setUser(MessageCreateEvent event, String user, String
+            userOutput) {
+        //        convert String to JSONarray
+        JSONArray resArr = new JSONArray(userOutput);
+//        then to a JSONobject
+        JSONObject osuUserRes = resArr.getJSONObject(0);
+        try {
+            MongoClient mongoClient = new MongoClient(new MongoClientURI
+                    ("mongodb://localhost:27018"));
+            DB database = mongoClient.getDB("npchan");
+            DBCollection userCol = database.getCollection("users");
+            DBObject osuUser = new BasicDBObject("_id", user).append("username",
+                    osuUserRes.getString("username")).append("osu_id",
+                    osuUserRes.getString("user_id"));
+            userCol.insert(osuUser);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void getUser(MessageCreateEvent event, String userId) {
+        try {
+            MongoClient mongoClient = new MongoClient(new MongoClientURI
+                    ("mongodb://localhost:27018"));
+            DB database = mongoClient.getDB("npchan");
+            DBCollection userCol = database.getCollection("users");
+
+            DBObject query = new BasicDBObject("_id", userId);
+            DBCursor cursor = userCol.find(query);
+            DBObject user = cursor.one();
+
+            Bot.osuUserEmbed(event, (String)user.get("_id"), (String)user
+                    .get("username"));
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void osuUserEmbed(MessageCreateEvent event, String userId,
+                                    String osuUser){
+        try {
+//            build httpclient object
+            HttpClient httpClient = HttpClientBuilder.create().build();
+//            api url GET for song data
+            HttpGet getRequest = new HttpGet(
+                    "https://osu.ppy.sh/api/get_user?u=" + osuUser + "&k=" +
+                            System.getenv("OSU_KEY"));
+            getRequest.addHeader("accept", "application/json");
+//            had to set headers
+            getRequest.setHeader("User-Agent", "Anonymous");
+            HttpResponse response = httpClient.execute(getRequest);
+
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+
+            String userOutput;
+            while ((userOutput = br.readLine()) != null){
+                JSONArray resArr = new JSONArray(userOutput);
+                JSONObject osuUserRes = resArr.getJSONObject(0);
+                System.out.println(osuUserRes);
+                String username = osuUserRes.getString("username");
+                String pp = osuUserRes.getString("pp_raw");
+                String rank = osuUserRes.getString("pp_rank");
+                String cRank = osuUserRes.getString("pp_country_rank");
+                String country = osuUserRes.getString("country").toLowerCase();
+
+
+
+                EmbedBuilder embed = new EmbedBuilder().setTitle(username +" " +
+                        ":flag_" + country +":")
+                        .setDescription("Rank: " + rank + "\nCountry Rank: " +
+                                cRank + "\nPP: " + pp)
+                        .setFooter("© Riker, Flo, & Tux")
+                        .setColor(Color.pink);
+                event.getChannel().sendMessage(embed);
+            }
+
+            httpClient.getConnectionManager().shutdown();
+
+        } catch (ClientProtocolException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/npchan/Main.java
+++ src/main/java/com/npchan/Main.java
@@ -1,22 +1,60 @@
 package com.npchan;
 
-
+import com.mongodb.*;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.user.*;
+
+import java.net.UnknownHostException;
 
 public class Main {
 
     public static void main (String[] args) {
-        new DiscordApiBuilder().setToken(System.getenv("BOT_TOKEN")).login().thenAccept(api -> {
 
-            api.addMessageCreateListener(event -> {
+            new DiscordApiBuilder().setToken(System.getenv("BOT_TOKEN")).login().thenAccept(api -> {
 
-                if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                api.addMessageCreateListener(event -> {
+
+                    try{
+                        if(event.getMessage().getContent().substring(0,1)
+                                .equalsIgnoreCase(">")){
+                            String msgArgs = event.getMessage().getContent()
+                                    .substring(1,10);
+                            String osuUsername = event.getMessage().getContent()
+                                    .substring(11);
+
+                            switch(msgArgs){
+                                case "npchanSet":
+                                    String user = event.getMessage().getAuthor()
+                                            .getIdAsString();
+                                    Bot.osuUserApiCall(event, user, osuUsername);
+                                    break;
+                            }
+                        }
+                    } catch (Throwable e){
+                        e.printStackTrace();
+                    }
+
+//                    this will be an acceptance of data not a ! command
+                    if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
 //                    this will be dynamic
-                    Bot.getBeatmap(event, "tux", "889322");
-                }
+                        Bot.getBeatmap(event, "tux", "889322");
+                    }
+
+                    if (event.getMessage().getContent().equalsIgnoreCase
+                            ("!osu")) {
+                        String userId = event.getMessage().getAuthor()
+                                .getIdAsString();
+                        Bot.getUser(event, userId);
+                    }
+
+
+//                    this will be dynamic
+//                        Bot.setUser(user, "889322");
+
+                });
+                System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
             });
-            System.out.println("You can invite the bot by using the following url: "+api.createBotInvite());
-        });
+
     }
 }
 
